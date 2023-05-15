da7d28720d10e63e7559b05fcd7c5979a080c896
Denzel Washington <turboswanson@MS-7B89-f0923ed1.localdomain>
2023-05-15 23:44:37 +0000

    modified capi.c

--- strings/capitalization.c
+++ strings/capitalization.c
@@ -2,6 +2,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+
+
+//     ***** THE FIRST WAY *****
+
+
+
 
 // int main(void)
 // {
@@ -28,45 +36,94 @@
 //     return 0;
 // }
 
-int main(void)
-{
-    char *s = malloc(1*sizeof(char));
-    int n = 0;
 
-    while(1)
-    {
-        scanf("%c",&s[n]);
 
-        if(s[n] == '\n')
-        {
-            break;
-        }
-        n++;
-        s = realloc(s,(n+1)*sizeof(char));
-    }
 
-    *(s+n) = '\0';
+    //     ***** THE SECOND WAY *****
 
+
+
+
+
+// int main(void)
+// {
+//     char *s = malloc(1*sizeof(char));
+//     int n = 0;
+
+//     while(1)
+//     {
+//         scanf("%c",&s[n]);
+
+//         if(s[n] == '\n')
+//         {
+//             break;
+//         }
+//         n++;
+//         s = realloc(s,(n+1)*sizeof(char));
+//     }
+
+//     *(s+n) = '\0';
+
+//     int flag = 1;
+//     for(int i = 0; i < n; i++){
+//     if(*(s+i) == ' '){
+//         flag = 1;
+//         printf("%c",*(s+i));
+//     }
+
+//     else if(flag){
+//         printf("%c",*(s+i)-32);
+//         flag = 0;
+//     }
+
+//     else{
+//         printf("%c",*(s+i));
+
+//     }
+
+//     }
+
+//     free(s);
+
+//     return 0;
+// }
+
+
+
+
+
+
+ //   ******* THE THIRD WAY  ******** 
+
+
+
+
+
+ int main(void)
+ {
+    char c;
     int flag = 1;
-    for(int i = 0; i < n; i++){
-    if(*(s+i) == ' '){
-        flag = 1;
-        printf("%c",*(s+i));
+
+
+    while((c= getchar()) != '\n')
+    {
+        if(isspace(c))
+        {   
+            putchar(c);
+            flag = 1;
+        }
+
+        else if(flag)
+        {
+            putchar(toupper(c));
+            flag = 0;            
+        }
+
+        else
+        {
+            putchar(c);
+        }
     }
-
-    else if(flag){
-        printf("%c",*(s+i)-32);
-        flag = 0;
-    }
-
-    else{
-        printf("%c",*(s+i));
-
-    }
-
-    }
-
-    free(s);
 
     return 0;
-}
+ }
